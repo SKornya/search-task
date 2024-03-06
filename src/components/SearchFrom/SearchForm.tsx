a105891edab65b4fff362e375e6e935611aba0a1
skornya <kornyakovS@gmail.com>
2024-03-06 16:13:15 +0300

    add form and input handlers

--- src/components/SearchFrom/SearchForm.tsx
+++ src/components/SearchFrom/SearchForm.tsx
@@ -1,12 +1,22 @@
-import { FunctionComponent } from "react";
-import "./styles.css";
+import { ChangeEvent, FormEvent, FunctionComponent, useState } from 'react';
+import './styles.css';
 
 export const SearchForm: FunctionComponent = () => {
+  const [inputValue, setInputValue] = useState('');
+
+  const changeInputHandler = (event: ChangeEvent<HTMLInputElement>): void => {
+    setInputValue(event.target.value);
+  };
+
+  const formSubmitHandler = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
+  };
+
   return (
     <div className="searchForm">
-      <form>
-        <input type="text" value="Terry" />
+      <form onSubmit={formSubmitHandler}>
+        <input type="text" value={inputValue} onChange={changeInputHandler} />
       </form>
     </div>
   );
-}
+};
